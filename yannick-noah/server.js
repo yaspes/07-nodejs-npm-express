d714e6da6dfbb81a6efee46b92414cbd921cbde8
Yaspesberro <yaspesberro@gmail.com>
2018-04-18 10:16:18 -0700

    Created and worked on express.js

---
+++ yannick-noah/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+// REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
+app.post('/articles', express.urlencoded(), function(request, response) {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(request.body);
+  response.send('Record posted to server!!');
+})
